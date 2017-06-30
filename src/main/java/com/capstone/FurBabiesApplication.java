65dddf8029045c046c9b32d2afc5320ba2ad1ba9
micahdisney <micah.n.disney@gmail.com>
2017-06-30 14:42:30 -0500

    initial commit

---
+++ src/main/java/com/capstone/FurBabiesApplication.java
@@ -0,0 +1,12 @@
+package com.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurBabiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurBabiesApplication.class, args);
+	}
+}
