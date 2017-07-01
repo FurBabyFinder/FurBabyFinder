6e8bb72e1353d90dc3a7b6250bc718cb68212440
melodytempleton <mtempletoncantu@gmail.com>
2017-06-30 20:49:35 -0500

    Add autowired to controller

--- src/main/java/com/capstone/controllers/PetsController.java
+++ src/main/java/com/capstone/controllers/PetsController.java
@@ -2,6 +2,7 @@ package com.capstone.controllers;
 
 import com.capstone.repositories.PetsRepository;
 import com.capstone.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -13,6 +14,7 @@ public class PetsController {
     private final PetsRepository petsRepository;
     private final UsersRepository usersRepository;
 
+    @Autowired
     public PetsController(PetsRepository petsRepository, UsersRepository usersRepository) {
         this.petsRepository = petsRepository;
         this.usersRepository = usersRepository;
