915c60bb1328ac787cb8658b7cb126b96f394f14
melodytempleton <mtempletoncantu@gmail.com>
2017-06-30 20:48:14 -0500

    Add Pets, Users, and UserRoles repositories and Pets Controller

---
+++ src/main/java/com/capstone/controllers/PetsController.java
@@ -0,0 +1,23 @@
+package com.capstone.controllers;
+
+import com.capstone.repositories.PetsRepository;
+import com.capstone.repositories.UsersRepository;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by melodytempleton on 6/30/17.
+ */
+@Controller
+public class PetsController {
+
+    private final PetsRepository petsRepository;
+    private final UsersRepository usersRepository;
+
+    public PetsController(PetsRepository petsRepository, UsersRepository usersRepository) {
+        this.petsRepository = petsRepository;
+        this.usersRepository = usersRepository;
+    }
+
+
+
+}
