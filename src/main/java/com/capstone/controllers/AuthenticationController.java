a8510b8ecfdd529f0b46cd2514f782b70eaf7b44
micahdisney <micah.n.disney@gmail.com>
2017-07-08 15:32:22 -0500

    added login and register to authentication controller

--- src/main/java/com/capstone/controllers/AuthenticationController.java
+++ src/main/java/com/capstone/controllers/AuthenticationController.java
@@ -1,7 +1,22 @@
 package com.capstone.controllers;
 
-/**
- * Created by micahdisney on 7/8/17.
- */
+import com.capstone.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class AuthenticationController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
 }
