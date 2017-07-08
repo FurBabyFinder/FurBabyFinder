797fe16feb05b90b44ba4ddbb4aaf4781deb10c4
micahdisney <micah.n.disney@gmail.com>
2017-07-08 15:58:05 -0500

    added UsersController

--- src/main/java/com/capstone/controllers/UsersController.java
+++ src/main/java/com/capstone/controllers/UsersController.java
@@ -1,7 +1,31 @@
 package com.capstone.controllers;
 
-/**
- * Created by micahdisney on 7/8/17.
- */
+import com.capstone.models.User;
+import com.capstone.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
 public class UsersController {
+
+
+    @Autowired
+    UsersRepository usersDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @PostMapping("register")
+    public String saveUser(@ModelAttribute User user){
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        usersDao.save(user);
+
+        return null;
+    }
 }
