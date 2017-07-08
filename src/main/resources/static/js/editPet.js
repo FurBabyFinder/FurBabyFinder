c4310357fd5e9db44d672a058452ab2014c8c94a
melodytempleton <mtempletoncantu@gmail.com>
2017-07-07 21:46:59 -0500

    Add view to find pet by name or id Update Controller for new view Add link in Admin drop down of navbar

--- src/main/resources/static/js/editPet.js
+++ src/main/resources/static/js/editPet.js
@@ -20,9 +20,7 @@ $(document).ready(function(){
 
       $('.profilePic').each(function(){
            var trueOrNot = $(this).val();
-           console.log(trueOrNot);
           if(trueOrNot == "true"){
-              console.log('pass');
               $(this).attr('checked', true);
           }
       })
@@ -35,11 +33,7 @@ $(document).ready(function(){
 
         $(classname).each(function () {
              id = idbase + number;
-            var that = this;
-             console.log("id name is " + id);
             $(this).attr("id", id);
-            console.log(this);
-            // that.id = id;
             number++;
         })
     }
@@ -83,6 +77,7 @@ $(document).ready(function(){
     checkFiltersPetHas("not_dog_friendly");
     checkFiltersPetHas("medical_foster");
     checkFiltersPetHas("microchipped");
+
 
 
 });
