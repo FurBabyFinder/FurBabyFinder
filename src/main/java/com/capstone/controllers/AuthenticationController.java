9f458c54284d298ca3486b0cca177eb7c1c6da99
melodytempleton <mtempletoncantu@gmail.com>
2017-07-10 21:16:51 -0500

    Fix update user to not break if no roles are removed Fix Search User to not brak of alpha characters in the id search field Continue working on edit pet to update pictures on existing images

--- src/main/java/com/capstone/controllers/AuthenticationController.java
+++ src/main/java/com/capstone/controllers/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.capstone.controllers;
 
 import com.capstone.models.User;
+import com.capstone.repositories.PetsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
-
+PetsRepository petsRepository;
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
@@ -17,6 +18,7 @@ public class AuthenticationController {
     @GetMapping("/register")
     public String showRegisterForm(Model model){
         model.addAttribute("user", new User());
+        model.addAttribute("list", petsRepository.findSpecies());
         return "register";
     }
 }
